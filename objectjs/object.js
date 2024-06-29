b01290ff5892523f2e26c7d322713a5cdcd22244
Khushbuddin Ansari <118537465+Khushbuddina@users.noreply.github.com>
2024-06-29 09:19:46 +0000

    save

---
+++ objectjs/object.js
@@ -0,0 +1,13 @@
+const JsUser = {
+    name: "Munna",
+    "Full Name": "Munna Ansari",
+    age:"16",
+    location: "Lucknow",
+    email: "munna@gmail.com",
+    isLoggedIn: false,
+    LastLoginDay: ["Monday","Saturday"]
+}
+
+console.log(JsUser.email);
+console.log(JsUser["email"]);
+console.log(JsUser["Full Name"]);
