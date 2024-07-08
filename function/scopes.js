1e48ec587de9bfb51620e39679a5c6ad4b91a4de
Khushbuddin Ansari <118537465+Khushbuddina@users.noreply.github.com>
2024-07-08 09:32:28 +0000

    update

---
+++ function/scopes.js
@@ -0,0 +1,32 @@
+
+
+if (true) {
+    let a = 10
+    const b = 20
+    var c = 30
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one(){
+    const username = "Munna"
+    function two(){
+        const website = "Eduzest"
+        console.log(username);
+    }
+    // console.log(website);
+    // two()
+}
+one()
+
+if(true){
+    const username = "Munna"
+    if (username ==="Munna") {
+        const website = " Eduzest"
+        console.log(username + website);
+    }
+    console.log(website);
+}
+console.log(username);
